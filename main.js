fe4093c40cda4a8f6bab64804daedb7c07ea7516
SamuelMenon <104702356+SamuelMenon@users.noreply.github.com>
2023-10-03 07:47:53 -0300

    n

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
 function tocar(idAudio) {
-    document.querySelector(ideAudio).play();
-    } 
+    document.querySelector(idAudio).play();
+}
 
-    const lista = document.querySelectorAll('.tecla');
+const lista = document.querySelectorAll('.tecla');
 
-    let contador = 0;
+let contador = 0;
 
-    while (contador < lista.length) {
-        const tecla = lista[contador];
-        const classe = tecla.classlist[1];
+while (contador < lista.length) {
+    const tecla = lista[contador];
+    const classe = tecla.classList[1];
 
-        const idSom = `#som_${classe}`;
-        tecla.onclick = function () {
-            tocar(idSom);
-        }
-
-        contador = contador + 1;
-
-        tecla.onkedown = function (evento) {
-            tecla.classList.add('ativa')
-            console.log(evento.code);
-        }
-        tecla.onkeyup = function () {
-            tecla.classList.remove ('ativa');
-        }
+    const idSom = `#som_${classe}`;
+    tecla.onclick = function () {
+        tocar(idSom);
     }
+
+    contador = contador + 1;
+
+    tecla.onkedown = function (evento) {
+        tecla.classList.add('ativa')
+        console.log(evento.code);
+    }
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+}
